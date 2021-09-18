dfcfdd704773f9449fca4b2775b65257467bf93b
Clinton Forster <clintonforster@gmail.com>
2021-09-18 11:00:47 +1000

    add seed files for db data and update room id's

---
+++ migrations/20210918002108_seed_rooms_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO rooms (room_name, created_at, updated_at) VALUES
+('Standard Suite', now(), now()),
+('Deluxe Suite', now(), now());
---
+++ migrations/20210918002924_seed_reservations_table.postgres.up.sql
@@ -0,0 +1,6 @@
+INSERT INTO reservations (first_name, last_name, email, phone, start_date, end_date, room_id, created_at, updated_at) VALUES
+('Clinton', 'Forster', 'clintonforster@gmail.com', '0431476296', '2021-09-16', '2021-09-17', 1, now(), now()),
+('Krysten', 'Forster', 'kricketory@gmail.com', '0418793452', '2021-09-15', '2021-09-18', 2, now(), now()),
+('John', 'Smith', 'john@smith.com', '123456879', '2021-09-22', '2021-09-23', 1, now(), now()),
+('Terry', 'Smith', 'terry@smith.com', '546789231', '2021-10-07', '2021-10-09', 2, now(), now()),
+('John', 'Stamos', 'john@stamos.com', '789123456', '2021-09-29', '2021-09-30', 2, now(), now());
