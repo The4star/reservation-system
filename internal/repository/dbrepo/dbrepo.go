09972bdb6ba31495ab5727ccfc1238c6fe3a5c08
Clinton Forster <clintonforster@gmail.com>
2021-09-19 10:16:41 +1000

    commit parent level changes

--- internal/repository/dbrepo/dbrepo.go
+++ internal/repository/dbrepo/dbrepo.go
@@ -7,9 +7,19 @@ import (
 	"github.com/the4star/reservation-system/internal/repository"
 )
 
+type testDBRepo struct {
+	App *config.AppConfig
+	DB  *sql.DB
+}
 type postgresDBRepo struct {
 	App *config.AppConfig
 	DB  *sql.DB
+}
+
+func NewTestingRepo(app *config.AppConfig) repository.DatabaseRepo {
+	return &testDBRepo{
+		App: app,
+	}
 }
 
 func NewPostgresRepo(db *sql.DB, app *config.AppConfig) repository.DatabaseRepo {
